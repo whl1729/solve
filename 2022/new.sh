04860dec2288f72c0a080ff4cf0031c9df4c649b
along <wuhualong@minieye.cc>
2022-06-11 12:06:13 +0800

    add Q123: understand cors error

--- 2022/new.sh
+++ 2022/new.sh
@@ -5,18 +5,17 @@ if [ $# = 0 ]; then
   exit 1
 fi
 
-file="$1"
-questions="2022_questions.md"
-headline=$(cat $questions | egrep ^- | head -1 | awk '{printf "# %s %s\n", $4, $6}')
-today=$(date +%Y-%m-%d)
+readonly FILE="$1"
+readonly QUESTIONS="2022_questions.md"
+readonly HEADLINE="$(cat ${QUESTIONS} | grep -E ^- | head -1 | sed 's/- \[ \] \([^ ]\+\) [^ ]\+ \(.*\)/# \1 \2/g')"
+readonly TODAY="$(date +%Y-%m-%d)"
 
-echo "$headline" > "$file"
-echo >> "$file"
-echo "## 版本说明" >> "$file"
-echo >> "$file"
-echo "| 时间 | 版本 | 说明 |" >> "$file"
-echo "| ---- | ---- | ---- |" >> "$file"
-echo "| $today | 1.0 | 初稿 |" >> "$file"
-echo >> "$file"
-echo "## 解答" >> "$file"
-echo >> "$file"
+echo -e "$HEADLINE" > "$FILE"
+echo >> "$FILE"
+echo "## 版本说明" >> "$FILE"
+echo >> "$FILE"
+echo "| 时间 | 版本 | 说明 |" >> "$FILE"
+echo "| ---- | ---- | ---- |" >> "$FILE"
+echo "| $TODAY | 1.0 | 初稿 |" >> "$FILE"
+echo >> "$FILE"
+echo "## 解答" >> "$FILE"
